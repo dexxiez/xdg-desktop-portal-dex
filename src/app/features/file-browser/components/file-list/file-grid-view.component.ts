bd239214c47241743c5f08e73f379d5fadee1775
Dexxiez <toby@boulton.net.au>
2024-12-09 18:38:32 +1100

    feat: start file browser

---
+++ src/app/features/file-browser/components/file-list/file-grid-view.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-file-grid-view",
+  imports: [CommonModule],
+  templateUrl: "./file-grid-view.component.html",
+  styleUrls: ["./file-grid-view.component.scss"],
+})
+export class FileGridViewComponent {}
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { IconComponent } from "../../shared/components/icon/icon.component";
+import { FileGridViewComponent } from "../../features/file-browser/components/file-list/file-grid-view.component";
 
 @Component({
   selector: "app-main-layout",
-  imports: [CommonModule, IconComponent],
+  imports: [CommonModule, IconComponent, FileGridViewComponent],
   templateUrl: "./main-layout.component.html",
   styleUrls: ["./main-layout.component.scss"],
 })
